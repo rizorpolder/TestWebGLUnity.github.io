9a1baa880c4875ea873293d0e2509d13513e8d26
k.knyazev <k.knyazev@bgf.games>
2024-05-02 19:18:07 +0300

    test build

---
+++ TemplateData/UnityProgress.js
@@ -0,0 +1,49 @@
+﻿function createProgressBlock() {
+    const unityElement = document.getElementById("unityContainer");
+    let progressElement = document.getElementById("progress-bar");
+    let fullElement = document.getElementById("progress-bar-full");
+    let emptyElement = document.getElementById("progress-bar-empty");
+
+    progressElement = document.createElement("div");
+    progressElement.id = "progress-bar";
+    progressElement.className = "progress";
+
+    boxElement = document.createElement("div");
+    boxElement.id = "box";
+    boxElement.className = "box";
+    progressElement.appendChild(boxElement);
+
+    emptyElement = document.createElement("div");
+    emptyElement.className = "empty";
+    emptyElement.id = "progress-bar-empty";
+    boxElement.appendChild(emptyElement);
+
+    fullElement = document.createElement("div");
+    fullElement.className = "full";
+    fullElement.id = "progress-bar-full";
+    emptyElement.appendChild(fullElement);
+
+    textElement = document.createElement("div");
+    textElement.className = "text";
+    textElement.id = "text-progress";
+    progressElement.appendChild(textElement);
+
+    unityElement.appendChild(progressElement);
+}
+
+function unityProgress(progress) {
+    let fullElement = document.getElementById("progress-bar-full");
+    let textElement = document.getElementById("text-progress");
+    if (fullElement !== null) {
+        let progress_value = Math.round(100 * progress) + "%";
+        fullElement.style.width = progress_value;
+        textElement.setAttribute('loading_text', progress_value);
+    }
+}
+
+function hideProgressBar(unityInstance) {
+    let progressElement = document.getElementById("progress-bar");
+    if (progressElement !== null)
+        progressElement.style.display = "none";
+    return unityInstance;
+}
---
+++ TemplateData/config.js
@@ -0,0 +1,12 @@
+
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/hell-client.loader.js";
+var config = {
+    dataUrl: buildUrl + "/hell-client.data.unityweb",
+    frameworkUrl: buildUrl + "/hell-client.framework.js.unityweb",
+    codeUrl: buildUrl + "/hell-client.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "BGF.GAMES",
+productName: "Lucy’s Ville",
+productVersion: "1.2.3",
+};
